b05a42bd24d4e1f84623c63dc6a1f9980402459d
Soul Reaver <s.reaver@outlook.com>
2018-11-29 01:29:40 +0600

    L8 Main

--- l8/main/js/script.js
+++ l8/main/js/script.js
@@ -33,5 +33,51 @@ window.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 	});
+	//timer
+	function addZeroToDate(date) {
+		if (date.toString().length == 1) {
+			return "0" + date.toString();
+		} else {
+			return date;
+		}
+	}
+	let deadline = "2018-11-29";
 
+	function getTimeRemaining(endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			hours = Math.floor((t / 1000 / 60 / 60));
+		//hours = Math.floor((t / 1000 / 60 / 60) % 24),
+		//days = Math.floor((t/(1000*60*60*24)));
+		return {
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			hours = timer.querySelector(".hours"),
+			minutes = timer.querySelector(".minutes"),
+			seconds = timer.querySelector(".seconds"),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+			hours.textContent = addZeroToDate(t.hours);
+			minutes.textContent = addZeroToDate(t.minutes);
+			seconds.textContent = addZeroToDate(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+				hours.textContent = "00";
+				minutes.textContent = "00";
+				seconds.textContent = "00";
+			}
+		}
+	}
+	setClock("timer", deadline);
 });
