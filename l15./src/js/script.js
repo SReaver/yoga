d198f06bccffa62a3389f2f09a02e09542be2bee
Soul Reaver <s.reaver@outlook.com>
2018-12-07 22:37:24 +0600

    L15 Fixed

--- l15./src/js/script.js
+++ l15./src/js/script.js
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 	});
-	//timer
+
 	let addZeroToDate = (date) => {
 		if (date.toString().length == 1) {
 			return "0" + date.toString();
@@ -51,8 +51,7 @@ window.addEventListener("DOMContentLoaded", function () {
 			seconds = Math.floor((t / 1000) % 60),
 			minutes = Math.floor((t / 1000 / 60) % 60),
 			hours = Math.floor((t / 1000 / 60 / 60));
-		//hours = Math.floor((t / 1000 / 60 / 60) % 24),
-		//days = Math.floor((t/(1000*60*60*24)));
+
 		return {
 			'total': t,
 			'hours': hours,
@@ -85,7 +84,7 @@ window.addEventListener("DOMContentLoaded", function () {
 	};
 	setClock("timer", deadline);
 
-	//Modal
+
 	let more = document.querySelector(".more"),
 		overlay = document.querySelector(".overlay"),
 		close = document.querySelector(".popup-close"),
@@ -124,7 +123,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		document.body.style.overflow = "";
 	});
 
-	//additional animation of Modal
+
 
 	function animate(element) {
 		let op = 0;
@@ -141,7 +140,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		requestAnimationFrame(step);
 	}
 
-	//Form
+
 	let message = {
 		loading: "Загрузка...",
 		success: "Спасибо! Скором мы с вами свяжемся",
@@ -154,7 +153,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		formContact = document.getElementById("form"),
 		inputs = document.querySelectorAll("input");
 	statusMessage.classList.add("status");
-	//проверка на ввод цифр и плюса в поле телефона
+
 	inputs.forEach(function (item) {
 		if (item.type == "tel") {
 			item.addEventListener("keypress", function (e) {
@@ -222,7 +221,7 @@ window.addEventListener("DOMContentLoaded", function () {
 				.then(() => clearInput());
 		});
 	});
-	//Плавная прокрутка
+
 	let menu = document.querySelectorAll("li a");
 	menu.forEach(function (item) {
 		item.addEventListener("click", function (event) {
@@ -234,7 +233,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		});
 	});
 
-	//Slider
+
 	let slideIndex = 1,
 		slides = document.querySelectorAll('.slider-item'),
 		prev = document.querySelector(".prev"),
@@ -298,7 +297,7 @@ window.addEventListener("DOMContentLoaded", function () {
 	});
 	showSlides(1);
 
-	//calculator
+
 	let persons = document.querySelectorAll(".counter-block-input")[0],
 		restDays = document.querySelectorAll(".counter-block-input")[1],
 		calcInputs = document.querySelectorAll(".counter-block-input"),
@@ -307,7 +306,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		total = 0;
 
 	totalValue.innerHTML = total;
-	//функция анимации чисел
+
 	function animateTotalSum(startCount, endCount, element) {
 		let totalValue = startCount;
 		let increament = totalValue < endCount ? 100 : -100;
@@ -319,7 +318,7 @@ window.addEventListener("DOMContentLoaded", function () {
 			}
 		}, 1);
 	}
-	//Функция подсчёта итоговой суммы
+
 	function totalSumCalc(rst, prs) {
 		if (+rst && +prs) {
 			total = (+rst + +prs) * 4000;
@@ -335,7 +334,7 @@ window.addEventListener("DOMContentLoaded", function () {
 			totalSumCalc(restDays.value, persons.value);
 		});
 	});
-	//провека ввода только цифр
+
 	calcInputs.forEach((item) => {
 		item.addEventListener("keypress", (e) => {
 			if (/\D/.test(e.key)) {
