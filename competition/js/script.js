bcfb33e8195eb26519253c41acf95204dfeea462
Soul Reaver <s.reaver@outlook.com>
2018-12-08 15:57:03 +0600

    L15 fixed

--- competition/js/script.js
+++ competition/js/script.js
@@ -1,70 +1,9 @@
-//let arr = [1, 2, 3, 4, 5];
-//console.log(arr);
+"use stict";
 
-let carz = [];
-let ttt = 1;
-//let wer;
-// function getData() {
-//     return new Promise(function (resolve, reject) {
-
-let request = new XMLHttpRequest();
-request.open('GET', 'js/cars.json');
-request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-carz = request.onreadystatechange = function () {
-    if (request.readyState === 4) {
-        if (request.status == 200 && request.readyState == 4) {
-            //console.log(typeof (request.response));
-            //wer = request.response;
-            //console.log(cars);
-            //console.log(wer);
-            //console.log(typeof (request.response));
-            //console.log(cars);
-            //console.log(JSON.parse(request.response));
-            return JSON.parse(request.response).cars;
-
-            /* JSON.parse(request.response).forEach(function (item) {
-                cars.push(item);
-                console.log(item);
-            });
-*/
-            //console.log(wer[1]);
-            // console.log(JSON.parse(request.response));
-            //resolve(request.response);
-        } else {
-            //reject();
-            console.log(request.response);
-        }
-    }
-    request.send();
-};
-
-console.log(carz);
-//console.log(carz[0]);
-//console.log(ttt);
-
-//console.log(cars[0]);
-//console.log(cars);
-
-// cars.forEach(function (item) {
-//     console.log("item: " + item);
-// });
-
-// for (let i = 0; i < cars.length; i++) {
-//     console.log(cars[i]);
-// }
-//console.log(cars[1].category);
-//setTimeout(console.log(wer), 5000);
-//console.log(wer);
-//     });
-// }
-/*
-getData().then((data) => {
-    let carz = Object.values(JSON.parse(data).cars);
-    //cars = carz.concat();
-
-    console.log(carz);
-}).catch((data) => console.log("Что-то пошло не так!" + data));
-*/
-// cars.forEach(function (item) {
-//     console.log(item);
-// })
+fetch('js/cars.json')
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        console.log(data.cars);
+    });
--- l15/src/js/script.js
+++ l15/src/js/script.js
@@ -156,15 +156,56 @@ window.addEventListener("DOMContentLoaded", function () {
 
 	inputs.forEach(function (item) {
 		if (item.type == "tel") {
-			item.addEventListener("keypress", function (e) {
-				if ((!/\d|\+/gm.test(e.key) && e.keyCode != 8) || item.value.length >= 12 && e.keyCode != 8) {
-					e.preventDefault();
-				}
-				if (e.key == '+' && item.value.length > 0) {
-					e.preventDefault();
-				}
+			let word = ['+', '7', '(', '_', '_', '_', ')', '_', '_', '_', '-', '_', '_', '-', '_', '_'];
+			item.setSelectionRange(word.indexOf('_'), 1);
+			item.addEventListener("keydown", function (e) {
+				let input = item;
+				let regFull = /\+\d{1,3}\(\d{3}\)\d{3}-\d{2}-\d{2}/ig;
+				let reg = /\d/;
 
+				input.value = word.join('');
+
+				if (reg.test(e.key) && word.indexOf('_') != -1) {
+					e.preventDefault();
+					e.returnValue = false;
+					word[word.indexOf('_')] = e.key;
+					input.value = word.join('');
+					if (word.indexOf('_') != -1) {
+						input.setSelectionRange(word.indexOf('_') + 1, word.indexOf('_'));
+					} else {
+						input.setSelectionRange(word.length, word.length);
+					}
+
+					//далее проверка и действие при нажатии на Backspace
+				} else if (e.keyCode == 8) {
+					e.preventDefault();
+					let pos = this.selectionStart;
+
+					if (word[this.selectionStart - 1] != '-' && word[this.selectionStart - 1] != ')' && word[this.selectionStart - 1] != '(') {
+						if (word[this.selectionStart - 1] == '+') {
+							e.returnValue = false;
+						} else {
+							word[this.selectionStart - 1] = '_';
+							input.value = word.join('');
+
+							input.setSelectionRange(pos - 1, pos - 1);
+						}
+					} else {
+
+						word[this.selectionStart - 2] = '_';
+						input.value = word.join('');
+						input.setSelectionRange(pos - 2, pos - 2);
+					}
+				} else {
+					e.returnValue = false;
+				}
+				if (regFull.test(input.value)) {
+					input.style.backgroundColor = "green";
+				} else if (!regFull.test(input.value)) {
+					input.style.backgroundColor = "";
+				}
 			});
+
 		}
 	});
 
